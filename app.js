ff84a742eecb381c340f16e9cecaf94a1bc28edf
Joris Falter <you@example.com>
2022-09-29 10:24:32 +0700

    depart and return

--- app.js
+++ app.js
@@ -13,15 +13,18 @@ app.use(bodyParser.urlencoded({
 }));
 
 // define variables
-let pageCounter = 0;
+// let pageCounter = 0;
 let originLocation = "Bangkok";
-let originAirport = "vtbs"
+let originAirport = "lexj"
+let originTimeZone = 'Europe/Madrid'
+// let originAirport = "vtbs"
+// let originTimeZone = 'Asia/Bangkok';
 const departureUrl = "https://aeroapi.flightaware.com/aeroapi/airports/" + originAirport + "/flights/scheduled_departures?type=Airline"
 const returnUrl = "https://aeroapi.flightaware.com/aeroapi/airports/" + originAirport + "/flights/scheduled_arrivals?type=Airline"
 // vtse = chumphon
 // vtbs = suvarnabhumi
 // vtsm = samui
-
+// lexj = santander
 
 const fireItAllUp = async () => {
     await mongoose.connect("mongodb+srv://joris-mongo:" + process.env.ATLAS_KEY + "@cluster1.dkcnhgi.mongodb.net/flightsDB", { useNewUrlParser: true, useUnifiedTopology: true }); 
@@ -54,17 +57,25 @@ const fireItAllUp = async () => {
     const Returnflight = mongoose.model('Returnflight',returnFlightSchema);
 
     // for now, delete the db when we rerun the query
-    // Departingflight.deleteMany({},function(err){if(err){console.log(err)} else if (!err){console.log("departures db deleted before start")}});
+    Departingflight.deleteMany({},function(err){if(err){console.log(err)} else if (!err){console.log("departures db deleted before start")}});
     Returnflight.deleteMany({},function(err){if(err){console.log(err)} else if (!err){console.log("return db deleted before start")}});
 
 
     // Initial API Call
-    // fetchAirportData(departureUrl, "departure");
-    fetchAirportData(returnUrl, "return");
+    fetchAirportData(departureUrl, "departure", 0);
+    // 
 
+    // const fetchDepartureAndArrival = async() => {
+        //await 
+        // fetchAirportData(departureUrl, "departure");
+        // fetchAirportData(returnUrl, "return");
+    // }
+    // fetchDepartureAndArrival();
 
     // Create the function for API Call 
-    function fetchAirportData(url, direction) {
+    // direction is either "departure" or "return"; url is either "departureUrl" or "returnUrl"
+    function fetchAirportData(url, direction, pageCounter) {
+        
         fetch(url, {
             method: 'GET',
             headers: {
@@ -82,7 +93,7 @@ const fireItAllUp = async () => {
                             // define variables with data from api
                             let arrivalAirport = data.scheduled_departures[i].destination.code_iata;
                             let departureTimeZulu = new Date(data.scheduled_departures[i].scheduled_out);
-                            let departureTimeLocal = departureTimeZulu.toLocaleString('en-GB', {timeZone: 'Asia/Bangkok'});
+                            let departureTimeLocal = departureTimeZulu.toLocaleString('en-GB', {timeZone: originTimeZone});
                             let flightNumber = data.scheduled_departures[i].ident_iata;
 
                             // put data in database
@@ -103,7 +114,7 @@ const fireItAllUp = async () => {
                             // define variables with data from api
                             let departureAirport = data.scheduled_arrivals[i].origin.code_iata;
                             let arrivalTimeZulu = new Date(data.scheduled_arrivals[i].scheduled_in);
-                            let arrivalTimeLocal = arrivalTimeZulu.toLocaleString('en-GB', {timeZone: 'Asia/Bangkok'});
+                            let arrivalTimeLocal = arrivalTimeZulu.toLocaleString('en-GB', {timeZone: originTimeZone});
                             let flightNumber = data.scheduled_arrivals[i].ident_iata;
 
                             // put data in database
@@ -135,20 +146,26 @@ const fireItAllUp = async () => {
                         const delayForRateLimitAndCallNextPage = async () => {
                             await setTimeout(1500);
                             console.log("Waited 15s");
-                            fetchAirportData(url, direction);
+                            fetchAirportData(url, direction, pageCounter);
                         }
                         delayForRateLimitAndCallNextPage();
                     }
                 } else {
                     // when we have all the information from all pages. We end up here.
                     
+                    // If we checked for departures, we will now check for returns
+                    if (direction === "departure"){
+                        fetchAirportData(returnUrl, "return", 0);
+                    } else {console.log("end of execution")}
+
+
                     // display the last flight in the query
                     // console.log(departingFlights[departingFlights.length - 1])
 
                     // final message
                     const delayForCheckingIfDbisUpdated = async () => {
-                        await setTimeout(5000);
-                        console.log("Waited 5s");
+                        await setTimeout(10000);
+                        console.log("Waited 10s to make sure db is updated");
                         countDocuments();
                     }
                     delayForCheckingIfDbisUpdated();
@@ -161,10 +178,9 @@ const fireItAllUp = async () => {
     const countDocuments = async () => {
         // console.log("number of entries " + await Departingflight.countDocuments({}));
         console.log("number of entries " + await Returnflight.countDocuments({}));
-        console.log("end of execution")
     }
 }
-fireItAllUp();
 
+fireItAllUp();
 
 
