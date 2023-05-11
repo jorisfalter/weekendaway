c28efb18d28eff3f63c3d3370a0ac1e5009d640b
Joris Falter <joris@jorisfalter.com>
2023-05-11 20:32:55 +0700

    x

--- matcher.js
+++ matcher.js
@@ -433,6 +433,10 @@ app.post("/", function (req, res) {
   });
 });
 
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log("listening");
 });
