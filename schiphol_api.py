5aa38434abe6b53480e32dd57d076bf9f7d42c5f
Joris Falter <joris@jorisfalter.com>
2025-01-16 14:10:18 +0100

    x

--- schiphol_api.py
+++ schiphol_api.py
@@ -14,86 +14,24 @@ import os
 
 def get_flight_data():
     api = FlightRadar24API()
-    # data = []
-    from pymongo import MongoClient
-    ca = certifi.where()
+
 
     # Load environment variables from .env file
     load_dotenv()
-    mongoPass = os.getenv("MONGO_ATLAS_PASS")
-
-    client = pymongo.MongoClient(
-        f'mongodb+srv://joris-a380:{mongoPass}@cluster0.1gi6i3v.mongodb.net/?retryWrites=true&w=majority&connectTimeoutMS=5000', tlsCAFile=ca)
-
-    db = client['a380flightsDb']
-    # collection = db['a380flightsCollection']
-    collection = db['a380flightsCollectionV2']
+    # bounds = api.get_bounds_by_point(52.3169, 4.7459, 50000) # schiphol coordinates + 50km radius - doesn't work
+    bounds = "54.4,2.3,51.4,7.2" # [north latitude, west longitude, east longitude, south latitude]
+    # bounds = api.get_bounds(zone)
 
 
-    for flight in api.get_flights(aircraft_type="A388"):
+
+    for flight in api.get_flights(bounds = bounds):
+    # for flight in api.get_flights(aircraft_type="A388"):
         flight_details = api.get_flight_details(flight)
 
-        # get all flight details
-        # print(json.dumps(flight_details,
-        #       sort_keys=True, indent=4)[0:25000])
 
-        if flight_details['airport']['destination'] is not None:
-            target_timezone_origin = flight_details['airport']['origin']['timezone']['name']
-            target_timezone_destination = flight_details['airport']['destination']['timezone']['name']
+        with open('flight_details.json', 'w') as f:
+            json.dump(flight_details, indent=2, fp=f)
 
-            # Unix timestamp
-            unix_dep_time = flight_details['time']['scheduled']['departure']
-            unix_arr_time = flight_details['time']['scheduled']['arrival']
-
-            # Convert Unix timestamp to datetime object
-            utc_dep_datetime = datetime.datetime.utcfromtimestamp(
-                unix_dep_time)
-            utc_arr_datetime = datetime.datetime.utcfromtimestamp(
-                unix_arr_time)
-
-            # Set the UTC time zone to the datetime object
-            utc_dep_datetime = utc_dep_datetime.replace(tzinfo=pytz.utc)
-            utc_arr_datetime = utc_arr_datetime.replace(tzinfo=pytz.utc)
-
-            # Convert to the target time zone
-            local_dep_datetime = utc_dep_datetime.astimezone(
-                pytz.timezone(target_timezone_origin))
-            local_arr_datetime = utc_arr_datetime.astimezone(
-                pytz.timezone(target_timezone_destination))
-
-            # print all details
-            # print(flight.__dict__)
-
-            now = datetime.datetime.now()
-
-            departureTimeLocal = local_dep_datetime.strftime('%H:%M')
-            arrivalTimeLocal = local_arr_datetime.strftime('%H:%M')
-            departureDow = local_dep_datetime.weekday()
-            arrivalDow = local_arr_datetime.weekday()
-
-
-            # data for database
-            dataOneFlight = {"loggingTime":now,"flightNumber": flight.number, "originIata": flight.origin_airport_iata,
-                             "destinationIata": flight.destination_airport_iata, "departureDatetimeLocal": local_dep_datetime, 
-                             "arrivalDatetimeLocal": local_arr_datetime, "departureTimeLocal":departureTimeLocal, "arrivalTimeLocal":arrivalTimeLocal, "departureDow":departureDow, "arrivalDow":arrivalDow}
-
-            result = collection.insert_one(dataOneFlight)
-
-            # dataOneFlight = [flight.number, flight.origin_airport_iata,
-            #  flight.destination_airport_iata, local_dep_datetime, local_arr_datetime]
-            # data.append(dataOneFlight)
-
-            print(flight.number, flight.origin_airport_iata,
-                  flight.destination_airport_iata, local_dep_datetime, departureTimeLocal, departureDow, local_arr_datetime, arrivalTimeLocal, arrivalDow)
-
-
-        else:
-            print("no destination")
-            # print(flight_details)
-
-        # break
-    # close db:
-    client.close()
 
 
 if __name__ == "__main__":
